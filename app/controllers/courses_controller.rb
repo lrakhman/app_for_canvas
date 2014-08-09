0a76ae5c886056cb6964269165dc30cfe3e0a854
lrakhman <lana.rakhman@gmail.com>
2014-08-09 18:03:09 -0500

    more css

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,9 @@
 class CoursesController < ApplicationController
 	def index
+		@courses = Course.get_all_courses(1)
+		@courses2 = Course.get_all_courses(2)
+		@courses3 = Course.get_all_courses(3)
+		@courses4 = Course.get_all_courses(4)
+		# @the_number_we_want = Course.get_all_courses(1).headers['link'].split(', ')[1]
 	end
 end
