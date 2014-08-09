65f0c31760facfdba478166168aef6f16966cb7e
lrakhman <lana.rakhman@gmail.com>
2014-08-09 18:42:33 -0500

    added enrollments routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :courses, only: [:index, :show]
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: [:index, :create]
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
