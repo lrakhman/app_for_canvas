93f76ee722258c1fba5c69850207e86b42d9dfad
lrakhman <lana.rakhman@gmail.com>
2014-08-09 17:06:48 -0500

    added root to index, added httparty methods to get and post to api

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,12 @@
-class Course < ActiveRecord::Base
+class Course
+	include HTTParty
+  base_uri 'http://canvas-api.herokuapp.com'
+
+  def self.get_all_courses(page)
+    get("/api/v1/courses?access_token=#{token}&page=#{page}")
+  end
+
+   def self.token
+    post("/api/v1/tokens")['token']
+  end
 end
