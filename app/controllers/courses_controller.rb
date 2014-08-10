9f350c702958e1850718823968da24d3eb2a19b9
lrakhman <lana.rakhman@gmail.com>
2014-08-09 21:32:08 -0500

    playing with httparty, not sure I need models

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,13 @@
+require 'net/http'
 class CoursesController < ApplicationController
+	include HTTParty
+  base_uri 'canvas-api.herokuapp.com'
+  default_params page: @page, access_token: '9be624b4d5206a178fc56921d5bf2c2a'
+
 	def index
-		@courses = Course.get_all_courses(1)
-		# @link = Course.get_all_courses(1).headers['link'].split(', ')[1]
+		@courses = Course.get_all_courses(@page)
+
+		# @link = @courses.headers['link'].split(', ')
 	end
 end
 
--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,7 @@ class Course
 	include HTTParty
   base_uri 'http://canvas-api.herokuapp.com'
 
+
   def self.get_all_courses(page)
     get("/api/v1/courses?access_token=#{token}&page=#{page}")
   end
