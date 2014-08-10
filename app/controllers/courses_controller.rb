e2e15da9de34499bde7b82459fa7c0178f18314f
lrakhman <lana.rakhman@gmail.com>
2014-08-09 21:51:03 -0500

    trying to parse link header

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,9 +1,15 @@
+require 'link_header'
+
 class CoursesController < ApplicationController
 
 	def index
 		@courses = Course.get_all_courses
 
-		# @link = @courses.headers['link'].split(', ')
+
+		@link = @courses.headers['link']
+
+		@parsed_link = LinkHeader.parse(@link).to_a
+
 	end
 end
 
