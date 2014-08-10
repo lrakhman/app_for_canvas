af46faea98c076376da485cceb637441774c3bf0
lrakhman <lana.rakhman@gmail.com>
2014-08-09 23:43:03 -0500

    working with link header

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,12 +4,16 @@ class CoursesController < ApplicationController
 
 	def index
 		@courses = Course.get_all_courses
-
-
 		@link = @courses.headers['link']
 
-		@parsed_link = LinkHeader.parse(@link).to_a
+		parsed_link = LinkHeader.parse(@link).to_a
+
+		parsed_link.map do |item|
+			
+		end	
 
 	end
+
+	
 end
 
