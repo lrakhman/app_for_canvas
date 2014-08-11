54e3e01b2818610174d23b200550bb11c2128db0
lrakhman <lana.rakhman@gmail.com>
2014-08-11 16:04:52 -0500

    final adjustments

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -5,8 +5,8 @@ class EnrollmentsController < ApplicationController
 	def create
 		course_id = params[:id]
 		enroll_response = HTTParty.post("http://canvas-api.herokuapp.com/api/v1/courses/#{course_id}/enrollments", query: {access_token: '9be624b4d5206a178fc56921d5bf2c2a'})
-		logger.info "#{course_id}******************"
-		logger.info "#{enroll_response}===================="
+		# logger.info "#{course_id}******************"
+		# logger.info "#{enroll_response}===================="
 		redirect_to root_path
 	end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course
 	include HTTParty
   base_uri 'canvas-api.herokuapp.com'
-  # these default params are not working
+  # these default params are not working:
   # default_params page: @page, access_token: '9be624b4d5206a178fc56921d5bf2c2a'
 
   def self.get_all_courses(page)
