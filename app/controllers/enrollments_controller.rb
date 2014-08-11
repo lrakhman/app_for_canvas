cdb5c8125b01e9b96f44a606344c2e05d9857555
lrakhman <lana.rakhman@gmail.com>
2014-08-11 15:36:53 -0500

    enrollments almost work

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -3,7 +3,7 @@ class EnrollmentsController < ApplicationController
 		
 	end
 	def create
-		course_id = params[:course_id]
+		course_id = params[:id]
 		enroll_response = HTTParty.post("http://canvas-api.herokuapp.com/api/v1/courses/#{course_id}/enrollments", query: {access_token: '9be624b4d5206a178fc56921d5bf2c2a'})
 		logger.info "#{course_id}******************"
 		logger.info "#{enroll_response}===================="
